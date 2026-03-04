d25252ec079aabfc1b2d6538e77ce60b4e5bf680
Graziano Casto <graziano.casto@outlook.com>
2026-03-04 15:56:22 -0500

    Update template.sh

--- akamas/template/template.sh
+++ akamas/template/template.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 bash /work/code/java-study/akamas/scripts/patch_petclinic.sh \
-  --cpu-request ${petclinic_container.cpu_request}m \
-  --cpu-limit ${petclinic_container.cpu_limit}m \
-  --memory-request ${petclinic_container.memory_request}Mi \
-  --memory-limit ${petclinic_container.memory_limit}Mi \
+  --cpu-request ${petclinic_container.cpu_request} \
+  --cpu-limit ${petclinic_container.cpu_limit} \
+  --memory-request ${petclinic_container.memory_request} \
+  --memory-limit ${petclinic_container.memory_limit} \
   --jvm-opts "${petclinic_jvm.*}" \
   --hpa-cpu-target ${petclinic_hpa.targetCPUUtilizationPercentage}
